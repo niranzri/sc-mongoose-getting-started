649ee78063f78eae3e7d9fff669a0c3e1e4438e5
Noelia <noeliairanzoribera@gmail.com>
2024-01-15 18:08:54 +0100

    imported mongoose to integrate data base and created a mongoose model

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const logger = require("morgan");
 const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const Book = require("./models/Book.model");
 
 const app = express();
 
@@ -21,3 +23,22 @@ app.get('/', (req, res) => {
 
 // START SERVER
 app.listen(3000, () => console.log("App listening on port 3000!"));
+
+// CONNECT TO A MONGODB USING MONGOOSE
+mongoose
+  .connect("mongodb://127.0.0.1:27017/mongoose-example-dev")
+  .then(x => console.log(`Connected to Database: "${x.connections[0].name}"`))
+  .catch(err => console.error("Error connecting to MongoDB", err));
+
+//  GET  /books - Retrieve all books from the database
+app.get("/books", (req, res) => {
+  Book.find({})
+    .then((books) => {
+      console.log("Retrieved books ->", books);
+      res.json(books);
+    })
+    .catch((error) => {
+      console.error("Error while retrieving books ->", error);
+      res.status(500).send({ error: "Failed to retrieve books" });
+    });
+});
